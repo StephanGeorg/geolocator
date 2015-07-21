0508d494ddf386528d5b66041c58e9afd3cb9e67
Stephan Georg <stephan@23karat.de>
2015-07-21 17:19:12 +0200

    Added speed calculation and dynamic watcher

--- src/geolocator.js
+++ src/geolocator.js
@@ -1,142 +1,62 @@
 function Geolocator () {
-    this.init();
 
+    // support
     this.status = -1;
     this.error  = '';
-    this.count = 0;
 
+    // watcher
+    this.count = 0;
     this.first = {};
     this.last = {};
 
+
+    this.init();
+
 }
 
-
+/**
+ *  Initialize the geolocator
+ *
+ **/
 Geolocator.prototype.init = function () {
 
   if (navigator.geolocation) {
-    // Supported!
 
+    // Supported!
     this.status = 1;
     var _this = this;
     var start_pos;
 
-
-    // the first method you need to know about is
-    // navigator.geolocation.getCurrentPosition()
-    // which is going to be called once, on page load!
-    // It'll initiate an asynchronous request to detect
-    // the user's position and call a success/error callback.
-
-    // Note, if it's the first time an app is trying
-    // to get the position of the user, then the browser
-    // is going to seek for permission to whether allow/deny.
-
     navigator.geolocation.getCurrentPosition(function(pos) {
-      // we'll also store the start position to track
-      // the distance travelled
-      start_pos = pos;
-      _this.first = start_pos;
-      _this.last = start_pos;
 
-      console.log(pos); // Go ahead an inspect the dumped "Geoposition" object in your console.
+      _this.first = pos;
+      _this.last = pos;
 
-      // ob.timestamp is the time at which the data was read.
-      // ob.coords contains the location data.
-      // ob.coords.latitude and ob.coords.longitude are
-      // the geographic coordinates in decimal degree.
-
-      // Let's show the position lat, long in the HTML
-      //$$('.start_lat')[0].innerHTML = pos.coords.latitude;
-      //$$('.start_long')[0].innerHTML = pos.coords.longitude;
-
-      console.log("Start: " + pos.coords.latitude + "," + pos.coords.longitude);
       document.getElementById("start-pos").innerHTML = "Start: " + Number(pos.coords.latitude).toFixed(5) + "," + Number(pos.coords.longitude).toFixed(5);
 
     }, function(error) {
 
       this.error = error;
-      console.log(error);
 
       switch (error.code) {
-        case 1:
-          // 1 === error.PERMISSION_DENIED
-          console.log('User does not want to share Geolocation data.');
-          break;
-
-        case 2:
-          // 2 === error.POSITION_UNAVAILABLE
-          console.log('Position of the device could not be determined.');
-          break;
-
-        case 3:
-          // 3 === error.TIMEOUT
-          console.log('Position Retrieval TIMEOUT.');
-          break;
-
-        default:
-          // 0 means UNKNOWN_ERROR
-          console.log('Unknown Error');
-          break;		}
-
+        case 1:   // 1 === error.PERMISSION_DENIED
+                  console.log('User does not want to share Geolocation data.');
+                  break;
+        case 2:   // 2 === error.POSITION_UNAVAILABLE
+                  console.log('Position of the device could not be determined.');
+                  break;
+        case 3:   // 3 === error.TIMEOUT
+                  console.log('Position Retrieval TIMEOUT.');
+                  break;
+        default:  // 0 means UNKNOWN_ERROR
+                  console.log('Unknown Error');
+                  break;
+      }
     });
 
-
-    // Great! Now we're the Geolocation API Masters :D
-
-    // Finally, we need to learn about error handling too!
-    // getCurrentPosition or watchPosition, both accepts
-    // a second argument which can be a callback fired
-    // if any error occurred. Let's see how.
-
-
-    // Time to learn about how we can watch/track position, now.
-
-    // If the position data changes or more accurate data arrives
-    // then we can setup a callback to fire with the
-    // watchPosition() function
-    // it accepts arguments similarly to getCurrentPosition()
-
-    // THe function returns an ID number that uniquely identifies
-    // the watcher!
-
-    var watch_id = navigator.geolocation.watchPosition(function(pos) {
-      // lat, long
-
-      _this.count++;
-
-      //$$('.cur_lat')[0].innerHTML = pos.coords.latitude;
-      //$$('.cur_long')[0].innerHTML = pos.coords.longitude;
-
-      console.log("Watching..." + pos.coords.latitude + "," + pos.coords.longitude);
-      document.getElementById("watching").innerHTML = "Watching " + _this.count  + ": " + Number(pos.coords.latitude).toFixed(4) + "," + Number(pos.coords.longitude).toFixed(4);
-
-      console.log("Distance: " + calculateDistance(start_pos.coords.latitude, start_pos.coords.longitude, pos.coords.latitude, pos.coords.longitude));
-      document.getElementById("distance").innerHTML = "Distance: " + Number(calculateDistance(start_pos.coords.latitude, start_pos.coords.longitude, pos.coords.latitude, pos.coords.longitude)).toFixed(3) + "km";
-
-      console.log("Time: " + (_this.last.timestamp - pos.timestamp));
-      document.getElementById("time").innerHTML = "Time: " + Number((pos.timestamp - _this.last.timestamp)/1000).toFixed(3) + "s";
-
-      _this.last = pos;
-
-      // So, how about calculating the distance travelled
-      // by the user from the start position to some 'x' position ?
-
-      // Quite easy, we're going to use a function to calculate
-      // the distance based on the start/end lat/long borrowed from:
-      // http://www.html5rocks.com/en/tutorials/geolocation/trip_meter/
-
-      //$$('.distance')[0].innerHTML = calculateDistance(start_pos.coords.latitude, start_pos.coords.longitude, pos.coords.latitude, pos.coords.longitude);
-
-      // Nice! If you got a handheld device, just move around
-      // and you'll see the Distance change in the output box!
-    });
-
-    // you can use the watch_id to stop tracking the user's
-    // position further by passing to clearWatch()
-    // similar to settimeout/cleartimeout or setInterval/clearInterval
-    // navigator.geolocation.clearWatch(watch_id);
-
-
+    // check speed
+    var watchSpeedId = _this.addWatcher(_this.calcSpeed);
+    this.status = 2;
 
 
   } else {
@@ -147,10 +67,56 @@ Geolocator.prototype.init = function () {
   }
 };
 
+/**
+*    Add a Watcher to track positions
+*
+**/
+Geolocator.prototype.addWatcher = function (cb) {
+  var _this = this;
+  if(navigator.geolocation) {
+    return navigator.geolocation.watchPosition(function(pos){
+      cb(pos, _this);
+    });
+  }
+};
+
+/**
+*   Calculate the speed from positions
+*
+**/
+Geolocator.prototype.calcSpeed = function (pos, _this) {
+
+  var distance = 0;
+  var speed = 0;
+  var time = 0;
+
+  _this.count++;
+
+  document.getElementById("watching").innerHTML = "Watching " + _this.count  + ": " + Number(pos.coords.latitude).toFixed(4) + "," + Number(pos.coords.longitude).toFixed(4);
+  document.getElementById("distance").innerHTML = "Distance: " + Number(calculateDistance(_this.first.coords.latitude, _this.first.coords.longitude, pos.coords.latitude, pos.coords.longitude)).toFixed(3) + "km";
+  document.getElementById("time").innerHTML = "Time: " + Number((pos.timestamp - _this.first.timestamp)/1000).toFixed(3) + "s";
+
+  distance = Number(calculateDistance(_this.first.coords.latitude, _this.first.coords.longitude, pos.coords.latitude, pos.coords.longitude)).toFixed(6);
+  time = Number((pos.timestamp - _this.first.timestamp)/1000).toFixed(6);
+
+  time = time / 60 / 24;
+
+  if(time > 0) {
+    speed = distance / t;
+    console.log("Distance: " + distance);
+    console.log("Speed: " + speed + "km/h");
+    document.getElementById("speed").innerHTML = "Speed: " + speed + " km/h";
+  }
+
+  _this.last = pos;
+
+};
+
+
+
 
 
 // Usefull methods
-
 function calculateDistance(lat1, lon1, lat2, lon2) {
   var R = 6371; // km
   var dLat = (lat2 - lat1).toRad();
